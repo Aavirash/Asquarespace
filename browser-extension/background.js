19f38f25aa40f8396b388b08429d9acecf905ed7
Aavirash <aavirash@gmail.com>
2026-05-05 03:03:28 +0200

    fix: wrap background handlers in try/catch, fix context menu

--- browser-extension/background.js
+++ browser-extension/background.js
@@ -1,17 +1,18 @@
 const SUPABASE_URL = 'https://seceezshzzkxoqllbosw.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_9uq_CY2KsPns9dtogTVbyA_5wlvxA3e';
-const STATE_TABLE = 'user_workspace_state';
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
-    id: 'save-to-asquarespace',
-    title: 'Save to Asquarespace',
-    contexts: ['image', 'page', 'link'],
+  chrome.contextMenus.removeAll(() => {
+    chrome.contextMenus.create({
+      id: 'save-to-asquarespace',
+      title: 'Save to Asquarespace',
+      contexts: ['image', 'page', 'link'],
+    });
   });
 });
 
 chrome.contextMenus.onClicked.addListener(async (info, tab) => {
-  if (info.menuItemId === 'save-to-asquarespace') {
+  if (info.menuItemId === 'save-to-asquarespace' && tab && tab.id) {
     let itemData = null;
     if (info.srcUrl) {
       itemData = { src: info.srcUrl, pageUrl: info.pageUrl, title: extractFileName(info.srcUrl) };
@@ -20,7 +21,11 @@ chrome.contextMenus.onClicked.addListener(async (info, tab) => {
     } else {
       itemData = { src: info.pageUrl, pageUrl: info.pageUrl, title: tab?.title || 'Web Page' };
     }
-    chrome.tabs.sendMessage(tab.id, { action: 'saveItem', item: itemData });
+    try {
+      chrome.tabs.sendMessage(tab.id, { action: 'saveItem', item: itemData });
+    } catch (e) {
+      // Tab might not have content script injected
+    }
   }
 });
 
@@ -30,59 +35,67 @@ function extractFileName(url) {
 }
 
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  if (msg.action === 'getAuth') {
-    chrome.storage.local.get(['asq_user', 'asq_token'], (result) => {
-      sendResponse({ user: result.asq_user || null, token: result.asq_token || null });
-    });
-    return true;
-  }
-  if (msg.action === 'setAuth') {
-    chrome.storage.local.set({ asq_user: msg.user, asq_token: msg.token }, () => {
+  try {
+    if (msg.action === 'getAuth') {
+      chrome.storage.local.get(['asq_user', 'asq_token'], (result) => {
+        sendResponse({ user: result.asq_user || null, token: result.asq_token || null });
+      });
+      return true;
+    }
+    if (msg.action === 'setAuth') {
+      chrome.storage.local.set({ asq_user: msg.user, asq_token: msg.token }, () => {
+        sendResponse({ ok: true });
+      });
+      return true;
+    }
+    if (msg.action === 'clearAuth') {
+      chrome.storage.local.remove(['asq_user', 'asq_token'], () => {
+        sendResponse({ ok: true });
+      });
+      return true;
+    }
+    if (msg.action === 'getPendingEmail') {
+      chrome.storage.local.get(['asq_pending_email'], (result) => {
+        sendResponse({ email: result.asq_pending_email || null });
+      });
+      return true;
+    }
+    if (msg.action === 'setPendingEmail') {
+      chrome.storage.local.set({ asq_pending_email: msg.email, asq_last_email: msg.email }, () => {
+        sendResponse({ ok: true });
+      });
+      return true;
+    }
+    if (msg.action === 'getLastEmail') {
+      chrome.storage.local.get(['asq_last_email'], (result) => {
+        sendResponse({ email: result.asq_last_email || null });
+      });
+      return true;
+    }
+    if (msg.action === 'clearPending') {
+      chrome.storage.local.remove(['asq_pending_email'], () => {
+        sendResponse({ ok: true });
+      });
+      return true;
+    }
+    if (msg.action === 'supabaseRequest') {
+      supabaseFetch(msg.endpoint, msg.options)
+        .then((data) => sendResponse(data))
+        .catch((err) => sendResponse({ error: err.message }));
+      return true;
+    }
+    if (msg.action === 'showToast' && sender.tab) {
+      try {
+        chrome.tabs.sendMessage(sender.tab.id, { action: 'showToast', message: msg.message });
+      } catch (e) {}
       sendResponse({ ok: true });
-    });
-    return true;
+      return true;
+    }
+  } catch (e) {
+    console.error('background onMessage error:', e);
+    sendResponse({ error: e.message });
   }
-  if (msg.action === 'clearAuth') {
-    chrome.storage.local.remove(['asq_user', 'asq_token'], () => {
-      sendResponse({ ok: true });
-    });
-    return true;
-  }
-  if (msg.action === 'getPendingEmail') {
-    chrome.storage.local.get(['asq_pending_email'], (result) => {
-      sendResponse({ email: result.asq_pending_email || null });
-    });
-    return true;
-  }
-  if (msg.action === 'setPendingEmail') {
-    chrome.storage.local.set({ asq_pending_email: msg.email, asq_last_email: msg.email }, () => {
-      sendResponse({ ok: true });
-    });
-    return true;
-  }
-  if (msg.action === 'getLastEmail') {
-    chrome.storage.local.get(['asq_last_email'], (result) => {
-      sendResponse({ email: result.asq_last_email || null });
-    });
-    return true;
-  }
-  if (msg.action === 'clearPending') {
-    chrome.storage.local.remove(['asq_pending_email'], () => {
-      sendResponse({ ok: true });
-    });
-    return true;
-  }
-  if (msg.action === 'supabaseRequest') {
-    supabaseFetch(msg.endpoint, msg.options).then(sendResponse).catch(err => {
-      sendResponse({ error: err.message });
-    });
-    return true;
-  }
-  if (msg.action === 'showToast') {
-    chrome.tabs.sendMessage(sender.tab.id, { action: 'showToast', message: msg.message });
-    sendResponse({ ok: true });
-    return true;
-  }
+  return false;
 });
 
 async function supabaseFetch(endpoint, options = {}) {
